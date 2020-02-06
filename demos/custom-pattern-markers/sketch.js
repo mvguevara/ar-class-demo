22a3f6bbe930e662bdff7a8d61bd3b848769927a
Martin Guevara <guevara.mrtn1@gmail.com>
2020-02-06 14:30:33 -0800

    Added files

---
+++ demos/custom-pattern-markers/sketch.js
@@ -0,0 +1,18 @@
+/* 
+This is a blank JavaScript sketch in P5! 
+We won't be using this for the AR demo, but this is a GREAT place to learn how to code.
+
+If you are interested in JavaScript, follow Dan Schiffman's YouTube Tutorial Series here:
+https://www.youtube.com/watch?v=yPWkPOfnGsw&list=PLRqwX-V7Uu6Zy51Q-x9tMWIv9cueOFTFA 
+
+For the AR demo, hit the bracket arrow ">" next to where it says "sketch.js" at the top left of this screen. Then open the index.html file.
+
+*/
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
